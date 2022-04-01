c147cd63eb1b1a48b5879f4f1c6c915f3c208ca8
Fariz Rifqi <farizrifqi26@gmail.com>
2022-04-01 22:55:05 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+let config = {
+  token: "mfa.XXXXXXX",
+  channels: [
+    "83XXXXX"
+    ],
+  message: {
+    title: "VALORANT",
+    link: "https://www.youtube.com/"
+  }
+ 
+}
+
+module.exports = config;
+
+
+
+
+
+
+
+
+
+
+
+
+
